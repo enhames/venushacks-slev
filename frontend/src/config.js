123121be978f66ba9919ab0fdc136b4ea9c44343
spencerleaf <therealspencerleaf@gmail.com>
2025-05-24 18:43:22 -0700

    config merge conflict

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
-const BASE_URL = 'https://localhost:3000';
-=======
 const BASE_URL = 'http://localhost:3000';
->>>>>>> fe9c9d014311ad0dc431444ea4751f93628cd475
 export default BASE_URL;
